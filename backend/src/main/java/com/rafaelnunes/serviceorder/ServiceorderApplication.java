c5f1825dd98fef5f57ae32099836d24b04957054
rafaelnunes34 <rafael.nunes34@gmail.com>
2023-01-12 20:05:27 -0300

    Projeto backend criado

---
+++ backend/src/main/java/com/rafaelnunes/serviceorder/ServiceorderApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelnunes.serviceorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceorderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rafaelnunes/serviceorder/ServiceorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelnunes.serviceorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
